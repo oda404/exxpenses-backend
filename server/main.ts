f25563a5e81f73dfc3531504983ebc1882ffde4c
oda404 <xdxalexandru404@gmail.com>
2022-10-27 19:15:27 +0300

    Add express-session and cors.

--- server/main.ts
+++ server/main.ts
@@ -7,6 +7,9 @@ import { UserResolver } from "../resolvers/user";
 import { createServer as createHttpServer } from "http";
 import { exxpensesDataSource } from "./data_source";
 import { customFormatError } from "./error_format";
+import session = require("express-session");
+import { ResolverContext } from "../resolvers/types";
+import cors = require("cors");
 
 async function new_apollo_server() {
     return new ApolloServer({
@@ -15,6 +18,7 @@ async function new_apollo_server() {
                 UserResolver
             ]
         }),
+        context: ({ req, res }): ResolverContext => ({ req, res }),
         formatError: customFormatError,
     });
 }
@@ -23,12 +27,32 @@ async function new_http_server() {
     /* Creat express server */
     const app = express();
 
+    app.use(cors({
+        origin: "http://localhost:3000",
+        credentials: true
+    }));
+
+    /* Cookies middleware */
+    app.use(session({
+        name: "user_session",
+        cookie: {
+            path: "/",
+            httpOnly: true,
+            secure: false,
+            maxAge: undefined,
+            sameSite: "lax"
+        },
+        secret: "penis secret",
+        resave: false,
+        saveUninitialized: false,
+    }));
+
     /* Create and start the apollo server */
     const apollo_middleware = await new_apollo_server()
     await apollo_middleware.start();
 
     /* Add apollo as a middleware to express */
-    apollo_middleware.applyMiddleware({ app })
+    apollo_middleware.applyMiddleware({ app, cors: false, path: "/" })
 
     return createHttpServer(app);
 }

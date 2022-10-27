01defba81543ee149251b89d52190399666490fc
oda404 <xdxalexandru404@gmail.com>
2022-10-27 19:19:23 +0300

    types: Add ResovlerContext + decorate GenericFieldError.

--- resolvers/types.ts
+++ resolvers/types.ts
@@ -1,10 +1,16 @@
 
 /* Used for letting the user know which field is wrong, if any. */
-import {Field, ObjectType} from "type-graphql";
+import { Field, ObjectType } from "type-graphql";
+import { Session } from "express-session";
+import { Request, Response } from "express";
+
+export type ResolverContext = {
+    req: Request & { session: Session & { userId?: number } };
+    res: Response;
+};
 
 @ObjectType()
-export class GenericFieldError
-{
+export class GenericFieldError {
     @Field()
     name: string;
 

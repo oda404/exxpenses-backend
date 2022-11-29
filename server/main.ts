9c96ded870400d8bc9d83383886938e6a1b5c4c6
oda404 <xdxalexandru404@gmail.com>
2022-11-29 16:20:32 +0200

    server: Add some initial logging about different server information.

--- server/main.ts
+++ server/main.ts
@@ -17,6 +17,7 @@ import connectRedis from "connect-redis";
 const RedisStore = connectRedis(session);
 import Redis from "ioredis";
 import { readFileSync } from "fs";
+import { exit } from "process";
 
 const env = process.env.ENV!;
 const port = process.env.PORT!;
@@ -31,6 +32,12 @@ const session_secret_path = process.env.SESSION_SECRET_PATH!;
 
 const https_key_path = process.env.HTTPS_KEY_PATH!;
 const https_cert_path = process.env.HTTPS_CERT_PATH!;
+
+const psql_host = process.env.PSQL_HOST!;
+const psql_port = process.env.PSQL_PORT!;
+const psql_db = process.env.PSQL_DB!;
+const psql_username = process.env.PSQL_USERNAME!;
+const psql_pass = process.env.PSQL_PASS!;
 
 async function new_apollo_server() {
     return new ApolloServer({
@@ -47,6 +54,11 @@ async function new_apollo_server() {
 }
 
 async function new_redis_store() {
+
+    const redis_pass_len = redis_pass.length;
+    console.log(`redis: Connecting to ${redis_host}:${redis_port}`);
+    console.log(`redis: Password length: ${redis_pass_len}, ending in "${redis_pass.substring(redis_pass_len - 3)}"`);
+
     let redisClient = new Redis({
         host: redis_host,
         port: Number(redis_port),
@@ -62,6 +74,17 @@ async function new_redis_store() {
 }
 
 async function new_http_server() {
+
+    const session_secret = readFileSync(session_secret_path).toString();
+
+    console.log(`session: domain: ${session_domain}`);
+    console.log(`session: secret length: ${session_secret.length}`);
+
+    if (env === "prod" && session_secret.length < 128) {
+        console.log("Server started in production mode but session secret is shorter than 128 characters!");
+        exit(1);
+    }
+
     /* Creat express server */
     const app = express();
     const redisStore = await new_redis_store();
@@ -98,6 +121,9 @@ async function new_http_server() {
     let server: any;
 
     if (env === "prod") {
+        console.log(`https: Reading ssl certificate from ${https_cert_path}`);
+        console.log(`https: Reading ssl certificate key from ${https_key_path}`);
+
         server = createHttpsServer(
             {
                 key: readFileSync(https_key_path),
@@ -114,6 +140,14 @@ async function new_http_server() {
 }
 
 async function main() {
+
+    console.log(`server: Environment is ${env}`);
+    console.log(`server: Server is set to start on ${port}`);
+    console.log(`server: Serving frontend ${frontend_url}`);
+
+    console.log(`psql: Connecting to ${psql_host}:${psql_port}`);
+    console.log(`psql: Connecting to database ${psql_db} as ${psql_username}`);
+    console.log(`psql: Password length: ${psql_pass.length}, ending in "${psql_pass.substring(psql_pass.length - 3)}"`);
 
     /* Initialize the connection to Postgresql */
     await exxpensesDataSource.initialize();

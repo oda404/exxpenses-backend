6d59904007df0410edd9d116665554b27c2dd6d4
oda404 <xdxalexandru404@gmail.com>
2023-04-27 11:45:09 +0100

    turnstile: Fix node version conflicts shit.

--- utils/turnstile.ts
+++ utils/turnstile.ts
@@ -1,11 +1,12 @@
 import { IncomingHttpHeaders } from "http";
+import fetch from "node-fetch";
 
 const turnstile_verify_url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
 const turnstile_managed_key = process.env.TURNSTILE_MANAGED_KEY!;
 
 export async function turnstile_verify_managed(token: string, headers: IncomingHttpHeaders) {
 
-    let form = new FormData();
+    let form = new URLSearchParams();
     form.append("secret", turnstile_managed_key);
     form.append("response", token);
     let ip = headers["CF-Connecting-IP"];
@@ -17,6 +18,6 @@ export async function turnstile_verify_managed(token: string, headers: IncomingH
         method: 'POST',
     });
 
-    const res = await result.json();
+    const res = await result.json() as any;
     return res["success"] === true;
 }
